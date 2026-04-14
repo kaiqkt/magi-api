21ab648b20b8caf7d93d538bc1b46e39b8dfc493
kaiqkt <kaiquebeserra1313@gmail.com>
2026-04-13 21:50:09 -0300

    admin or owner can associate a github account to a project

---
+++ src/test/kotlin/com/kaiqkt/magiapi/integration/resources/GithubHelper.kt
@@ -0,0 +1,39 @@
+package com.kaiqkt.magiapi.integration.resources
+
+import com.kaiqkt.magiapi.resources.github.responses.GithubUserResponse
+import com.kaiqkt.magiapi.unit.resources.MockServerHolder
+import org.mockserver.model.HttpRequest
+import org.mockserver.model.HttpResponse
+import org.springframework.http.HttpStatus
+
+object GithubHelper : MockServerHolder() {
+    override fun domainPath(): String = "/user"
+
+    fun mockGetUserSuccessfully(response: GithubUserResponse) {
+        mockServer()
+            .`when`(
+                HttpRequest.request()
+                    .withMethod("POST")
+                    .withPath("/user"),
+            )
+            .respond(
+                HttpResponse.response()
+                    .withStatusCode(HttpStatus.OK.value())
+                    .withContentType(org.mockserver.model.MediaType.APPLICATION_JSON)
+                    .withBody(objectMapper.writeValueAsString(response)),
+            )
+    }
+
+    fun mockGetUserUnauthorized() {
+        mockServer()
+            .`when`(
+                HttpRequest.request()
+                    .withMethod("POST")
+                    .withPath("/user"),
+            )
+            .respond(
+                HttpResponse.response()
+                    .withStatusCode(HttpStatus.UNAUTHORIZED.value()),
+            )
+    }
+}
